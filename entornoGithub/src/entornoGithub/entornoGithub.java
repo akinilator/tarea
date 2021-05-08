5c50efdb9d27312586971886f38c1095151013d8
adrij <adrij@DESKTOP-S0BEN7T>
2021-05-08 10:55:59 +0200

    version 1del fichero

--- entornoGithub/src/entornoGithub/entornoGithub.java
+++ entornoGithub/src/entornoGithub/entornoGithub.java
@@ -27,7 +27,7 @@ public class entornoGithub extends Application {
 	public void start(Stage stage) {
 
 
-//VERSION 11111111111111111111111111111111111111111111111111111111111111111111111111111111111	
+//VERSION 11111111111111111111111111111111111111111111111111111111111111111111111111111111111
 		BorderPane borde = new BorderPane();
 
 		//creamos un grid pane
@@ -65,7 +65,6 @@ public class entornoGithub extends Application {
 		Button enviar = new Button("enviar");
 
 		//FORMULARIO*******************************************************************
-
 		//contraseña
 		PasswordField passwordField = new PasswordField();
 		passwordField.setPromptText("contraseña: ");
@@ -113,8 +112,8 @@ public class entornoGithub extends Application {
 
 		stage.setScene(scene);
 		stage.show();
-		
-		
+
+
 
 
 }
